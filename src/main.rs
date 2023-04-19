fe47fe60ac16c1e652412348ed3f727e212f42ee
badmood111 <petrzela.adam@seznam.cz>
2023-04-19 07:48:11 +0200

    finish the guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,45 @@
 use rand::Rng; //random number generator trait (must be in scope for methods to work)
+use std::cmp::Ordering;
 use std::io; //standard library //third party crate
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100); //1..=100 - end of Range is inclusive
-    println!("The secret number is {}.", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    //creates a new instance of a String - growable UTF-8 encoded text
-    // :: - Associated Function (function implemented on a type)
-    let mut guess = String::new();
+        //creates a new instance of a String - growable UTF-8 encoded text
+        // :: - Associated Function (function implemented on a type)
+        let mut guess = String::new();
 
-    //input/output::standard_input()
-    io::stdin()
-        //(...) - where to append the input
-        //&mut guess - mutable reference (like a copy without memory use)
-        .read_line(&mut guess)
-        //called on the read_line return value Result
-        .expect("Failed to read line");
+        io::stdin()
+            //(...) - where to append the input
+            //&mut guess - mutable reference (like a copy without memory use)
+            .read_line(&mut guess)
+            //called on the read_line return value Result
+            .expect("Failed to read line");
+        println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
+        //trim and parse guess into an int
+        //shadowing guess
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            //_ is a catchall value - we match all Error values with all contents inside of them
+            Err(_) => continue,
+        };
+
+        //Compare using match and .cmp()
+        //match compares all options and breaks after a successful match
+        match guess.cmp(&secret_number) {
+            //Ordering enum results of .cmp()
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Less => println!("Too low!"),
+        }
+    }
 }
